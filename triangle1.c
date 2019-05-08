0bbc084a3a0d9d9838c6c285e2c81be92ca8d888
Prashanthreddyk99 <50397730+Prashanthreddyk99@users.noreply.github.com>
2019-05-08 15:40:18 +0530

    Update triangle1.c

--- triangle1.c
+++ triangle1.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
- 
-int main()
-{
-  int row, c, n=5, s;
- 
-  
- 
-  s = n;
- 
-  for (row = 1; row <= n; row++)  // Loop to print rows
-  {
-    for (c = 1; c < s; c++)  // Loop to print spaces in a row
-      printf(" ");
- 
-    s--;
- 
-    for (c = 1; c <= 2*row - 1; c++) // Loop to print stars in a row
-      printf("*");
- 
-    printf("\n");
-  }
- 
-  return 0;
+#include <conio.h>
+void main() {
+	int i,j;
+	clrscr();
+	for (i=0; i<5; i++) {
+		for (j=0; j<5; j++) {
+			printf(" * ");
+		}
+		printf("\n");
+	}
+	getch();
 }
